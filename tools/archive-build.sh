6caff5f68387b6abccad169da5baf7caaf35c213
Lukaswnd <Lukas+git@windeln.nrw>
2024-06-07 14:41:17 +0200

    try double release

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -5,15 +5,16 @@ IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || git -C "$IDF_PATH" 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 
 archive_path="dist/arduino-esp32-libs-$1-$idf_version_string.tar.gz"
-pio_zip_archive_path="dist/framework-arduinoespressif32.zip"
-pio_zip_archive_libs_path="dist/framework-arduinoespressif32-libs.zip"
+pio_zip_archive_path="/framework-arduinoespressif32.zip"
+pio_zip_archive_libs_path="/framework-arduinoespressif32-libs.zip"
+pio_zip_archive_short="/build.zip"
 
 mkdir -p dist && rm -rf "$archive_path"
 if [ -d "out" ]; then
 	cd out && tar zcf "../$archive_path" * && cd ..
 fi
 
-cd out
+cd out 
 echo "Creating PlatformIO Tasmota framework-arduinoespressif32"
 mkdir -p arduino-esp32/cores/esp32
 mkdir -p arduino-esp32/tools/partitions
@@ -33,13 +34,40 @@ rm -rf arduino-esp32/libraries/SPIFFS
 rm -rf arduino-esp32/libraries/ESP_SR
 rm -rf arduino-esp32/tools/esp32-arduino-libs
 rm -rf arduino-esp32/tools/gen_insights_package.py
-cp -rf tools/esp32-arduino-libs arduino-esp32/tools/
+
 cp ../package.json arduino-esp32/package.json
 cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
-cp -rf arduino-esp32/tools/esp32-arduino-libs framework-arduinoespressif32-libs/
+cp -rf tools/esp32-arduino-libs framework-arduinoespressif32-libs/
 cp -rf arduino-esp32/ framework-arduinoespressif32/
+
+
+cp -rf tools/esp32-arduino-libs arduino-esp32/tools/
+# Use sed to replace the line
+sed -i '/^FRAMEWORK_LIBS_DIR = /c\FRAMEWORK_SDK_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "arduino-esp32/tools/platformio-build.py"
+# Check if the sed command was successful
+if [ $? -eq 0 ]; then
+  echo "File updated successfully."
+else
+  echo "Failed to update the file."
+fi
+find "arduino-esp32/tools/esp32-arduino-libs" -type f -name "platformio-build.py" | while read -r FILE_PATH; do
+  # Use sed to replace the line
+  sed -i '/^FRAMEWORK_LIBS_DIR = /c\FRAMEWORK_SDK_DIR = join(FRAMEWORK_DIR, "tools", "esp32-arduino-libs")' "$FILE_PATH"
+
+  # Check if the sed command was successful
+  if [ $? -eq 0 ]; then
+    echo "File $FILE_PATH updated successfully."
+  else
+    echo "Failed to update the file $FILE_PATH."
+    exit 1
+  fi
+done
+
+cp -rf arduino-esp32/ framework-arduinoespressif32-build/
 
 # If the framework is needed as tar.gz uncomment next line
 # tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
-7z a -mx=9 -tzip -xr'!.*' ../$pio_zip_archive_path framework-arduinoespressif32/
-7z a -mx=9 -tzip -xr'!.*' ../$pio_zip_archive_libs_path framework-arduinoespressif32-libs/
+
+7z a -mx=9 -tzip -xr'!.*' ../dist/$pio_zip_archive_path framework-arduinoespressif32/
+7z a -mx=9 -tzip -xr'!.*' ../dist/$pio_zip_archive_libs_path framework-arduinoespressif32-libs/
+7z a -mx=9 -tzip -xr'!.*' ../dist/$pio_zip_archive_short framework-arduinoespressif32-build/
